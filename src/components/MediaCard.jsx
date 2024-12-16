8f88692fe644a545f0006323e873807b55417fb1
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-12-16 17:23:46 +0100

    add flag for language

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 export default function MediaCard({ media }) {
+  const originalLanguage = media.original_language;
+
+  const languageFlag = `https://flagsapi.com/${originalLanguage}/flat/24.png`;
+
+  const [flagError, setFlagError] = useState(false);
+
+  const handleFlagError = () => {
+    setFlagError(true);
+  };
+
   return (
     <>
       <ul>
         <li>{media.name || media.title}</li>
         <li>{media.original_name || media.original_title}</li>
-        <li>{media.original_language}</li>
+        <li>
+          {flagError ? (
+            <span>{originalLanguage}</span>
+          ) : (
+            <img
+              src={languageFlag}
+              alt={originalLanguage}
+              onError={handleFlagError}
+            />
+          )}
+        </li>
         <li>{media.vote_average}</li>
       </ul>
     </>
