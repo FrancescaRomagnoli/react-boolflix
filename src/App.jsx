2d6142cfb0360ab9e4db9da1bb95b8ee29553306
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-12-16 13:39:39 +0100

    add fetch on button click

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState("");
 
-  const handleSearchTerm = (event) => {
+  const apiUrl = "https://api.themoviedb.org/3/search/movie?";
+
+  const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    // console.log(searchTerm);
   };
 
   const handleSearchButton = (event) => {
     event.preventDefault();
     console.log(searchTerm);
+
+    const apiUrl = import.meta.env.VITE_THEMOVIEDB_API_URL;
+    const apiKey = import.meta.env.VITE_THEMOVIEDB_API_KEY;
+
+    const url = `${apiUrl}search/movie?api_key=${apiKey}&query=${searchTerm}&language=it-IT`;
+    console.log(url);
+
+    fetch(`${url}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
@@ -25,7 +39,7 @@ function App() {
               <input
                 type="search"
                 value={searchTerm}
-                onChange={handleSearchTerm}
+                onChange={handleSearch}
                 placeholder="search..."
               />
               <button>search</button>
